a95eac66e325625c35490d40138f7a8fe8ddfc04
xoyoc <xoyoc_l2@hotmail.com>
2026-01-29 16:58:46 -0600

    update settings

--- checador/settings.py
+++ checador/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = get_env('SECRET_KEY', default='')
 """ try:
 except Exception:
     raise ValueError('SECRET_KEY must be set in .env file or environment variables')
